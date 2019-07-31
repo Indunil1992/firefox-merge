88b16334ecede24df2af749289f701dbf7668e3e
Indunil1992 <indunil@adroitlogic.com>
2019-07-31 12:09:41 +0530

    7777

--- firefox-merge/lambda.js
+++ firefox-merge/lambda.js
@@ -1,30 +1,37 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const ses = new AWS.SES();
 
 exports.handler = function (event, context, callback) {
-    s3.putObject({
-        "Body": "test",
-        "Bucket": "indunil1",
-        "Key": "1.jpg",
-        "Tagging": "tg1=tgv1&tg2=tgv2",
-        "Metadata": {
-            "mt1": "mtv2"
-        }
-    })
-        .promise()
-        .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-            }
-            */
-        })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
 
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Data: `<p>
+      My Bonnie lies over the ocean.
+    
+      My Bonnie lies over the sea.
+    
+      My Bonnie lies over the ocean.
+    
+      Oh, bring back my Bonnie to me.
+    </p>`
+                }
+            },
+            Subject: {
+                Data: 'test 31/7/2019'
+            }
+        },
+        Source: 'hirudinee+aws@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
 
     callback(null, { "message": "Successfully executed 1.3.2" });
