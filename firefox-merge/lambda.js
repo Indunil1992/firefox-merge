da9d6421a87e9de2add8f187e8fb1aea87251d43
Indunil1992 <indunil@adroitlogic.com>
2019-07-31 12:16:36 +0530

    mkj

--- firefox-merge/lambda.js
+++ firefox-merge/lambda.js
@@ -1,30 +1,21 @@
 let AWS = require('aws-sdk');
-const ses = new AWS.SES();
+const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
 
-    ses.sendEmail({
-        Destination: {
-            ToAddresses: ['indunil@adroitlogic.com'],
-            CcAddresses: [],
-            BccAddresses: []
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Data: `<p>This is a paragraph.</p>
-<p>This is another paragraph.</p>`
-                }
-            },
-            Subject: {
-                Data: 'test 31/7/2019 prt 2'
-            }
-        },
-        Source: 'hirudinee+aws@adroitlogic.com'
-    }, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-    });
+    sns.publish({
+        Message: 'vvvvv',
+        Subject: 'vv',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:rr'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
 
     callback(null, { "message": "Successfully executed 1.3.2" });
