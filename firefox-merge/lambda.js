8b9648f9d3a79f707ce79448962871411af3ee0c
Indunil1992 <indunil@adroitlogic.com>
2019-07-31 12:04:15 +0530

    7

--- firefox-merge/lambda.js
+++ firefox-merge/lambda.js
@@ -1,21 +1,29 @@
 let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-const sqs = new SL_AWS.SQS(AWS);
+const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-
-    sqs.sendMessage({
-        MessageBody: '123',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/test.fifo`,
-        DelaySeconds: '0',
-        MessageDeduplicationId: '1',
-        MessageGroupId: '12',
-        MessageAttributes: {}
-    }, function (data) {
-        // your logic (logging etc) to handle successful message delivery, should be here
-    }, function (error) {
-        // your logic (logging etc) to handle failures, should be here
-    });
+    s3.putObject({
+        "Body": "test",
+        "Bucket": "indunil1",
+        "Key": "1.jpg",
+        "Tagging": "tg1=tgv1&tg2=tgv2",
+        "Metadata": {
+            "mt1": "mtv2"
+        }
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
 
 
