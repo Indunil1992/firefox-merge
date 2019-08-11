fdcc8c813b28f633ab7342ac2c28d3466e3975ba
Indunil1992 <indunil@adroitlogic.com>
2019-08-11 08:00:28 +0530

    z

--- firefox-merge/lambda.js
+++ firefox-merge/lambda.js
@@ -1,22 +1,7 @@
-let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-
-
 exports.handler = function (event, context, callback) {
 
-    kinesis.putRecord({
-        Data: '1',
-        PartitionKey: '1',
-        StreamName: 'testindunil'
-    }).promise()
-        .then(data => {
-            // your logic goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
 
-
+    
 
     callback(null, { "message": "Successfully executed 1.3.2" });
 }
